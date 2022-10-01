842dd9e630cfcdb83d63a49c6e09153abfcaa624
Ardian <ardiaei099@gmail.com>
2022-10-01 15:38:15 +0700

    first commit

---
+++ basicconcept_mathOperator_3_incrementDecrement.js
@@ -0,0 +1,28 @@
+//post increment
+var a = 0
+var b = 10
+var a = b++
+console.log(a)
+console.log(b)
+
+//pre increment
+var a = 0
+var b = 10
+var a = ++b
+console.log(a)
+console.log(b)
+console.log("========DECREMENT========")
+
+//post decrement
+var a = 0
+var b = 10
+var a = b--
+console.log(a)
+console.log(b)
+console.log("========INCREMENT========")
+//pre decrement
+var a = 0
+var b = 10
+var a = --b
+console.log(a)
+console.log(b)
